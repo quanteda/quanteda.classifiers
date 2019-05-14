14cbd99d48f35732705b7605fd874891e31c003c
kbenoit <kbenoit@lse.ac.uk>
2019-05-14 11:29:07 +1000

    Tidy up data Rd, factorize some docvars
    - corrects the man entries for docvar data types
    - adds a package Rd entry
    - changes some UK corpus docvars to factors
    - tidies up Rd formatting

---
+++ R/quanteda.classifiers-package.R
@@ -0,0 +1,12 @@
+#' quanteda.classifiers
+#' 
+#' Extensions to \pkg{quanteda} that provide supervised machine learning models
+#' for document-feature matrices.
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
