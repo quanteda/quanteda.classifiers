b3356112c2126730c3adb6febe6c6321a9f6785b
Patrick Chester <pjc468@nyu.edu>
2019-05-24 01:57:58 -0400

    Add parameters and allow conversion of tokens to sequences for both main function and predict function

--- R/textmodel_cnnlstmemb.R
+++ R/textmodel_cnnlstmemb.R
@@ -5,9 +5,23 @@
 #' @inheritParams textmodel_svm
 #' @param units The number of network nodes used in the first layer of the
 #'   sequential model
-#' @param dropout A floating variable bound between 0 and 1. It determines the
+#' @param dropout1 A floating variable bound between 0 and 1. It determines the
 #'   rate at which units are dropped for the linear transformation of the
 #'   inputs.
+#' @param dropout2 A floating variable bound between 0 and 1. It determines the
+#'   rate at which units are dropped for the linear transformation of the
+#'   inputs.
+#' @param dropout3 A floating variable bound between 0 and 1. It determines the
+#'   rate at which units are dropped for the linear transformation of the
+#'   inputs.
+#' @param wordembeddim The number of word embedding dimensions to be fit
+#' @param filter 
+#' @param kernel_size 
+#' @param pool_size Size of the max pooling windows.
+#'   \code{\link[keras]{layer_max_pooling_1d}}
+#' @param units_lstm The number of nodes of the lstm layer
+#' @param words The maximum number of words used to train model. Defaults to the number of features in \code{x}
+#' @param maxsenlen The maximum sentence length of training data 
 #' @param optimizer optimizer used to fit model to training data, see
 #'   \code{\link[keras]{compile.keras.engine.training.Model}}
 #' @param loss objective loss function, see
@@ -19,6 +33,7 @@
 #' @keywords textmodel
 #' @importFrom keras keras_model_sequential to_categorical
 #' @importFrom keras layer_dense layer_activation layer_dropout compile fit
+#' @importFrom keras layer_embedding layer_conv_1d layer_max_pooling_1d layer_lstm
 #' @export
 #' @examples 
 #' \dontrun{
@@ -29,36 +44,37 @@
 #'     corpus_sample(size = ndoc(corpcoded))
 #' corp <- corpcoded + corpuncoded
 #' 
-#' # form a tf-idf-weighted dfm
-#' dfmat <- dfm(corp) %>%
-#'     dfm_tfidf()
+#' # form a tokens object
+#' corptok <- tokens(texts(corp))
 #' 
 #' set.seed(1000)
-#' tmod <- textmodel_cnnlstmemb(dfmat, y = docvars(dfmat, "crowd_immigration_label"),
+#' tmod <- textmodel_cnnlstmemb(corptok, y = docvars(dfmat, "crowd_immigration_label"),
 #'                         epochs = 5, verbose = 1)
 #' pred <- predict(tmod, newdata = dfm_subset(dfmat, is.na(crowd_immigration_label)))
 #' table(pred)
 #' tail(texts(corpuncoded)[pred == "Immigration"], 10)
 #' }
-textmodel_cnnlstmemb <- function(x, y, units = 512, dropout = .2, 
-                            optimizer = "adam",
-                            loss = "categorical_crossentropy", 
-                            metrics = "categorical_accuracy",
-                            ...) {
+textmodel_cnnlstmemb <- function(x, y, units = 512, dropout1 = .2, dropout2 = .2, dropout3 = .2, 
+                                 wordembeddim = 30, filter = 48, kernel_size = 5, pool_size = 4,
+                                 units_lstm = 128, words = NULL, maxsenlen = 50,
+                                 optimizer = "adam",
+                                 loss = "categorical_crossentropy", 
+                                 metrics = "categorical_accuracy", ...) {
     UseMethod("textmodel_cnnlstmemb")
 }
 
 #' @export
-textmodel_cnnlstmemb.dfm <- function(x, y, units = 512, dropout = .2, 
+textmodel_cnnlstmemb.tokens <- function(x, y, units = 512, dropout1 = .2, dropout2 = .2, dropout3 = .2, 
+                                     wordembeddim = 30, filter = 48, kernel_size = 5, pool_size = 4,
+                                     units_lstm = 128, words = NULL, maxsenlen = 50, 
                                 optimizer = "adam",
                                 loss = "categorical_crossentropy", 
                                 metrics = "categorical_accuracy", ...) {
     stopifnot(ndoc(x) == length(y))
     
-    x <- as.dfm(x)
     y <- as.factor(y)
     result <- list(x = x, y = y, call = match.call(), classnames = levels(y))
-    
+
     # trim missings for fitting model
     na_ind <- which(is.na(y))
     if (length(na_ind) > 0) {
@@ -66,23 +82,27 @@ textmodel_cnnlstmemb.dfm <- function(x, y, units = 512, dropout = .2,
         y <- y[-na_ind]
         x <- x[-na_ind]
     }
+
+    x <- tokens2sequences(x, maxsenlen = maxsenlen, keepn = words)
     
+    if (is.null(words)) {words = x$nfeatures}    
     # "one-hot" encode y
     y2 <- to_categorical(as.integer(y) - 1, num_classes = nlevels(y))
     
     # use keras to fit the model
     model <- keras_model_sequential()
     model %>%
-        layer_dense(units = units, input_shape = nfeat(x)) %>%
-        layer_activation(activation = "relu") %>%
-        layer_dropout(rate = dropout) %>%
-        layer_dense(units = nlevels(y)) %>%
-        layer_activation(activation = "softmax")
+        layer_embedding(input_dim = words + 1, output_dim = wordembeddim, input_length = dim(x)[2]) %>%
+        layer_dropout(rate = dropout1) %>% 
+        layer_conv_1d(filters = filter, kernel_size = kernel_size, activation='relu') %>% 
+        layer_max_pooling_1d(pool_size = pool_size) %>% 
+        layer_lstm(units = units_lstm, dropout = dropout2, recurrent_dropout = dropout3) %>% 
+        layer_dense(units = nlevels(y), activation = 'sigmoid')
     compile(model, loss = loss, optimizer = optimizer, metrics = metrics)
-    history <- fit(model, x, y2, ...)
+    history <- fit(model, x$matrix, y2, ...)
     
     # compile, class, and return the result
-    result <- c(result, list(seqfitted = model))
+    result <- c(result, nfeatures = x$nfeatures, list(seqfitted = model))
     class(result) <- c("textmodel_cnnlstmemb", "textmodel", "list")
     return(result)
 }
@@ -113,15 +133,11 @@ predict.textmodel_cnnlstmemb <- function(object, newdata = NULL,
     type <- match.arg(type)
     
     if (!is.null(newdata)) {
-        data <- as.dfm(newdata)
+        data <- tokens2sequences(newdata, maxsenlen = ncol(object$matrix), keepn = object$nfeatures)
+        t2s_object <- tokens2sequences(object$x, maxsenlen = ncol(object$matrix), keepn = object$nfeatures)
+        data <- t2s_conform(data, t2s_object)
     } else {
-        data <- as.dfm(object$x)
-    }
-    
-    data <- if (is.null(newdata)) {
-        suppressWarnings(quanteda:::force_conformance(data, featnames(data), force))
-    } else {
-        quanteda:::force_conformance(data, featnames(data), force)
+        data <- tokens2sequences(object$x, maxsenlen = ncol(object$matrix), keepn = object$nfeatures)
     }
     
     if (type == "class") {
