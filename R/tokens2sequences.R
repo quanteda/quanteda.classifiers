1269134ee909ebfa746df0bf5959581880c1c42b
kbenoit <kbenoit@lse.ac.uk>
2019-05-29 16:33:10 +1000

    Fix documentation problem, lint

--- R/tokens2sequences.R
+++ R/tokens2sequences.R
@@ -1,20 +1,19 @@
-#' convert \code{\link[quanteda]{tokens}} to sequences for \pkg{quanteda.classifiers} lstm model input
+#' Convert quanteda tokens to keras sequences
 #'
-#' This function converts a \pkg{quanteda} \code{\link[quanteda]{tokens}} object into a \code{\link{tokens2sequence}} object. 
-#' 
-#' 
+#' This function converts a \pkg{quanteda} \code{\link[quanteda]{tokens}} object
+#' into a tokens sequence object as expected by some functions in the
+#' \pkg{keras} package.
 #' @param x \code{\link[quanteda]{tokens}} object
 #' @param maxsenlen the maximum sentence length kept in output matrix
 #' @param keepn the maximum number of features to keep
-#'
-#' @return \code{\link{tokens2sequences}} The output matrix has a 
-#' number of rows which represent each tokenized sentence input into the function
-#' and a number of columns determined by \code{maxsenlen}. The matrix contains a numeric 
-#' code for every unique token kept (determined by \code{keepn})
-#' and they are arranged in the same sequence indicated by the original \code{\link[quanteda]{tokens}} object. 
+#' @return \code{\link{tokens2sequences}} The output matrix has a number of rows
+#'   which represent each tokenized sentence input into the function and a
+#'   number of columns determined by \code{maxsenlen}. The matrix contains a
+#'   numeric code for every unique token kept (determined by \code{keepn}) and
+#'   they are arranged in the same sequence indicated by the original
+#'   \code{\link[quanteda]{tokens}} object.
 #' @export
 #' @examples
-#' \dontrun{
 #' corpcoded <- corpus_subset(data_corpus_manifestosentsUK, !is.na(crowd_immigration_label))
 #' corpuncoded <- data_corpus_manifestosentsUK %>%
 #'     corpus_subset(is.na(crowd_immigration_label) & year > 1980) %>%
@@ -25,8 +24,6 @@
 #' print(corp)
 #' seqs <- tokens2sequences(corptok, maxsenlen = 200)
 #' print(seqs)
-#' }
-#' 
 tokens2sequences <- function(x, maxsenlen = 40, keepn = NULL) {
     UseMethod("tokens2sequences")
 }
@@ -38,7 +35,10 @@ tokens2sequences.tokens <- function(x, maxsenlen = 40, keepn = NULL) {
     doc_nam <- docnames(x)
     x <- unclass(x)
     features <- attr(x, "types")
-    data <- data.frame(features = features, label1 = 1:length(features), freq = as.integer(tfeq[features]), stringsAsFactors = F)
+    data <- data.frame(features = features,
+                       label1 = 1:length(features),
+                       freq = as.integer(tfeq[features]),
+                       stringsAsFactors = FALSE)
     attributes(x) <- NULL
     data <- data[order(data$freq, decreasing = T), ]
     if (!is.null(keepn)) {
@@ -51,11 +51,17 @@ tokens2sequences.tokens <- function(x, maxsenlen = 40, keepn = NULL) {
     }
     data <- data[order(data$label1, decreasing = F), ]
     x <- lapply(x, function(y) as.integer(na.omit(data$label[y])))
-    mat <- do.call("rbind", lapply(x, function(y) if(length(y) >= maxsenlen) {y[1:maxsenlen]} else {c(rep(0,times = maxsenlen - length(y)), y)}))
+    mat <- do.call("rbind", lapply(x, function(y)
+        if (length(y) >= maxsenlen)
+            y[1:maxsenlen]
+        else
+            c(rep(0, times = maxsenlen - length(y)), y)
+        ))
     rownames(mat) <- doc_nam
     colnames(mat) <- as.character(1:maxsenlen)
     data <- data[!is.na(data$label), ]
-    data <- data[order(data$label, decreasing = F), c("features", "label", "freq")]
+    data <- data[order(data$label, decreasing = FALSE),
+                 c("features", "label", "freq")]
     rownames(data) <- NULL
     output <- list(matrix = mat, nfeatures = nrow(data), features = data)
     class(output) <- c("tokens2sequences", "list")
@@ -70,55 +76,59 @@ print.tokens2sequences <- function(x, ...) {
     # calculate % sparse
     zeros <- sum(colSums(x$matrix == 0))
     tot <- nrow(x$matrix) * ncol(x$matrix)
-    sparse_pct <- round(zeros/tot * 100, 1)
-    
+    sparse_pct <- round(zeros / tot * 100, 1)
+
     # determine max number of features to print
     max_n <- ifelse(ncol(x$matrix) > 10, 10, ncol(x$matrix))
-    
+
     # output
-    cat("Ordered feature matrix of: ", format(nrow(x$matrix), big.mark = ","), " documents, ", format(x$nfeatures, big.mark = ","), " features ", "(", sparse_pct, "% sparse).\n", sep = "")
-    cat(nrow(x$matrix), " x ", ncol(x$matrix), " Matrix of class \"tokens2sequences\" \n", sep = "")
+    cat("Ordered feature matrix of: ", format(nrow(x$matrix), big.mark = ","),
+        " documents, ", format(x$nfeatures, big.mark = ","), " features ",
+        "(", sparse_pct, "% sparse).\n", sep = "")
+    cat(nrow(x$matrix), " x ", ncol(x$matrix),
+        " Matrix of class \"tokens2sequences\" \n", sep = "")
     head(x$matrix[, 1:max_n], 4)
 }
 
-#' Converts the feature names of one \code{\link{tokens2sequence}} object to those of another
+#' Match the feature names of one tokens2sequences object to another
 #'
-#' 
-#' @param x \code{\link{tokens2sequence}} object that will be forced to conform
-#' @param y \code{\link{tokens2sequence}} object whose feature names will be used to change token labels for \code{x}
+#' Converts the feature names of one tokens2sequences object to those of
+#' another.  Useful in aligning training and test sets.
+#' @param x \code{\link{tokens2sequences}} object that will be forced to conform
+#' @param y \code{\link{tokens2sequences}} object whose feature names will be
+#'   used to change token labels for \code{x}
 #' @seealso \code{\link{tokens2sequences}}
 #' @export
 #' @examples 
-#' \dontrun{
 #' corpcoded <- corpus_subset(data_corpus_manifestosentsUK, !is.na(crowd_immigration_label))
 #' corpuncoded <- data_corpus_manifestosentsUK %>%
 #'     corpus_subset(is.na(crowd_immigration_label) & year > 1980) %>%
 #'     corpus_sample(size = ndoc(corpcoded))
 #' 
-#' tokx <- tokens(texts(corpuncoded))
-#' toky <- tokens(texts(corpcoded))
+#' tokx <- tokens(corpuncoded)
+#' toky <- tokens(corpcoded)
 #' 
 #' seqx <- tokens2sequences(tokx, maxsenlen = 50, keepn = 5000)
 #' seqy <- tokens2sequences(toky, maxsenlen = 50, keepn = 5000)
-#' 
-#' seqxy <- tokens2sequences_conform(seqx, seqy)
-#' 
-#' print(seqxy)
-#' }
-#' 
-
+#' tokens2sequences_conform(seqx, seqy)
 tokens2sequences_conform <- function(x, y) {
     UseMethod("tokens2sequences_conform")
 }
 
 #' @export
-#' @method tokens2sequences_conform tokens2sequences
 tokens2sequences_conform.tokens2sequences <- function(x, y) {
     stopifnot(is.tokens2sequences(x) & is.tokens2sequences(y))
-    joint_feat <- merge(x$features, y$features[, -3], by = "features", all.x = T)
-    joint_feat <- joint_feat[order(joint_feat$label.x, decreasing = F), ]
-    mat <- apply(x$matrix, 1, function(x) as.integer(na.omit(joint_feat$label.y[x])))
-    mat <- do.call("rbind", lapply(mat, function(y) if(length(y) >= ncol(x$matrix)) {y[1:ncol(x$matrix)]} else {c(rep(0,times = ncol(x$matrix) - length(y)), y)}))
+    joint_feat <- merge(x$features, y$features[, -3], by = "features",
+                        all.x = TRUE)
+    joint_feat <- joint_feat[order(joint_feat$label.x, decreasing = FALSE), ]
+    mat <- apply(x$matrix, 1,
+                 function(x) as.integer(na.omit(joint_feat$label.y[x])))
+    mat <- do.call("rbind", lapply(mat, function(y)
+        if (length(y) >= ncol(x$matrix))
+            y[1:ncol(x$matrix)]
+        else
+            c(rep(0, times = ncol(x$matrix) - length(y)), y)
+    ))
     rownames(mat) <- rownames(x$matrix)
     colnames(mat) <- colnames(x$matrix)
     joint_feat <- joint_feat[, c("features", "label.y", "freq")]
@@ -126,20 +136,19 @@ tokens2sequences_conform.tokens2sequences <- function(x, y) {
     joint_feat <- joint_feat[order(joint_feat$label, decreasing = F), ]
     joint_feat <- joint_feat[!is.na(joint_feat$label), ]
     rownames(joint_feat) <- NULL
-    
-    output <- list(matrix = mat, nfeatures = nrow(joint_feat), features = joint_feat)
+
+    output <-
+        list(matrix = mat, nfeatures = nrow(joint_feat), features = joint_feat)
     class(output) <- c("tokens2sequences", "list")
     return(output)
 }
 
-
-#' Checks to see if function is a \code{\link{tokens2sequences}} type
+#' Check to see if function is a tokens2sequences type
 #'
 #' 
 #' @param x Object that will be checked to see if it is of the type \code{\link{tokens2sequences}}
 #' @seealso \code{\link{tokens2sequences}}
 #' @export
-
 is.tokens2sequences <- function(x) {
     "tokens2sequences" %in% class(x)
 }
