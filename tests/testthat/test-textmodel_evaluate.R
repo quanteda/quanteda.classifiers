cddd855f054c8993eb40eaf27b0284fe5a0cfa34
Patrick Chester <pjc468@nyu.edu>
2020-05-27 17:33:26 +0900

    Add tests for textmodel_cnnlstmemb evaluation by textmodel_evaluate

--- tests/testthat/test-textmodel_evaluate.R
+++ tests/testthat/test-textmodel_evaluate.R
@@ -27,4 +27,16 @@ test_that("textmodel_evaluate works", {
     model_eval3 <- textmodel_evaluate(x = dfmat, y = labels, model = "textmodel_mlp", fun = "recall", k = 2, seed = 5, by_class = TRUE)
     expect_true("class" %in% names(model_eval3)) 
     expect_true(sum(levels(labels) %in% model_eval3$class) == 3)
+    
+    # Check if it works with textmodel_cnnlstm
+    
+    corp_tok <- corpus_sample(data_corpus_EPcoaldebate, size = 1000, by = "crowd_subsidy_label")
+    tok <- tokens(corp_tok)
+    labels <- docvars(corp_tok, "crowd_subsidy_label")
+    model_eval4 <- textmodel_evaluate(x = tok, y = labels, model = "textmodel_cnnlstmemb", fun = "f1_score", k = 3, seed = 5)
+    expect_equal(dim(model_eval4), c(3, 4))
+    expect_equal(names(model_eval4), c("k", "f1_score", "time", "seed"))
+    expect_equal(max(model_eval4$k), 3)
+    expect_condition(min(model_eval4$f1_score) > 0.1 & max(model_eval4$f1_score < 1))
     })
+
