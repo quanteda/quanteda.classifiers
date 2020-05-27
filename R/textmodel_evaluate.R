4beed93f6c4caa1e5251005fc1864269aba27112
Patrick Chester <pjc468@nyu.edu>
2020-05-27 17:32:59 +0900

    Add textmodel_cnnlstmemb support for textmodel_evaluate

--- R/textmodel_evaluate.R
+++ R/textmodel_evaluate.R
@@ -100,6 +100,60 @@ textmodel_evaluate.dfm <- function(x, y, model, fun = "f1_score", k = 5, paramet
     return(output)
 }
 
+#' @export
+textmodel_evaluate.tokens <- function(x, y, model, fun = "f1_score", k = 5, parameters = list(), seed = as.numeric(Sys.time()), time = TRUE, by_class = FALSE) {
+    if("accuracy" %in% fun & by_class){
+        cat("No class oriented accuracy score defined. Calculating average accuracy accross all classes.\n")
+    }
+    total_start <- Sys.time()
+    set.seed(seed)
+    y <- as.factor(y)
+    folds <- cut(seq(1,length(y)),breaks=k, labels=FALSE)
+    folds <- sample(folds, length(folds), replace = FALSE)
+    output <- list()
+    params_df <- expand.grid(parameters, stringsAsFactors = FALSE)
+    param_len <- ifelse(length(parameters) != 0, nrow(params_df), 1)
+    w <- 1
+    for(t in 1:param_len) {
+        param_list <- as.list(params_df[t, , drop = FALSE]) # drop = FALSE ensures that params_df remains a data.frame even if there is only a single input parameter
+        for(i in 1:k){
+            test_set <- which(folds == i)
+            x_train <- x[-test_set]
+            y_train <- y[-test_set]
+            x_test <- x[test_set]
+            y_test <- y[test_set]
+            start <- Sys.time()
+            mod <- do.call(what = model, args = c(list(x = x_train, y = y_train), param_list))
+            time <- round(as.numeric(difftime(Sys.time(), start, units = "secs")), 2) # Outputs time in seconds
+            names(time) <- "time"
+            y_pred <- predict(mod, x_test)
+            met <- lapply(fun, function(x) do.call(what = x, args = list(y_pred, y_test, by_class))) # Accepts any evaluation function that takes predicted and test vectors as inputs
+            #met <- as.list(met)
+            if(is.null(names(met))) {names(met) <- fun}
+            if(length(parameters) != 0){
+                output[[w]] <- data.frame(k = i, met, param_list, as.list(time), seed)
+            } else {
+                output[[w]] <- data.frame(k = i, met, as.list(time), seed)
+            }
+            if(by_class) {
+                output[[w]]$class <- rownames(output[[w]])
+            }
+            w <- w + 1
+        }
+    }
+    output <- do.call(rbind, output)
+    rownames(output) <- NULL
+    total_time <- round(as.numeric(difftime(Sys.time(), total_start, units = "secs")), 2) # Outputs time in seconds
+    attr(output, "model") <- model
+    attr(output, "fun") <- fun
+    attr(output, "k") <- k
+    attr(output, "parameters") <- parameters
+    attr(output, "nparameters") <- param_len
+    attr(output, "total_time") <- total_time
+    class(output) <- c("textmodel_evaluate", "data.frame")
+    return(output)
+}
+
 #' @seealso [textmodel_evaluate()]
 #' @importFrom utils head
 #' @method head textmodel_evaluate
@@ -233,7 +287,6 @@ recall <- function(pred, true, by_class = FALSE, ...){
 #' @param true a vector of known labels that are used to evaluate model performance
 #' @param by_class instead of returning the average accuracy across all labels, this function will return the balanced accuracy score for every class of the true vector
 #' @param ... additional arguments
-#' @aliases 
 #' @seealso [textmodel_evaluate()]
 #' @export
 
@@ -257,7 +310,6 @@ balanced_accuracy <- function(pred, true, by_class = FALSE, ...){
 #' which is being subjected to evaluation
 #' @param true a vector of known labels that are used to evaluate model performance
 #' @param ... additional arguments
-#' @aliases 
 #' @seealso [textmodel_evaluate()]
 #' @export
 
