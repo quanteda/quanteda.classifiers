e8529018f80b2fbce65fb5b05d18eba3ff28584a
Patrick Chester <pjc468@nyu.edu>
2021-06-02 02:02:00 -0400

    Add support for textmodel_cnnlstmemb to textmodel_evaluate

--- R/textmodel_evaluate.R
+++ R/textmodel_evaluate.R
@@ -5,7 +5,7 @@
 #' multiple parameter values are provided, the function will perform a grid
 #' search by estimating a separate model for every combination of parameters.
 #'
-#' @param x the \link{dfm} or \link{tokens} object on which the model will be
+#' @param x the \link{dfm}, \link{tokens}, or \link{tokens2sequences} object on which the model will be
 #'   fit.  Does not need to contain only the training documents.
 #' @param y vector of training labels associated with each document identified
 #'   in \code{train}.  (These will be converted to factors if not already
@@ -52,11 +52,11 @@ textmodel_evaluate.dfm <- function(x, y, model, fun = "f1_score", k = 5,
                                    parameters = list(),
                                    seed = as.numeric(Sys.time()),
                                    time = TRUE, by_class = FALSE) {
-    stopifnot(is.dfm(x))
+    correct_format <- (is.dfm(x))
+    stopifnot(correct_format)
     if ("accuracy" %in% fun & by_class) {
         cat("No class oriented accuracy score defined. Calculating average accuracy accross all classes.\n")
     }
-    if(is.tokens2sequences(x)) x <- x$matrix
     total_start <- Sys.time()
     set.seed(seed)
     y <- as.factor(y)
@@ -73,8 +73,8 @@ textmodel_evaluate.dfm <- function(x, y, model, fun = "f1_score", k = 5,
         for (i in 1:k) {
             test_set <- which(folds == i)
             x_train <- x[-test_set, ]
-            y_train <- y[-test_set]
             x_test <- x[test_set, ]
+            y_train <- y[-test_set]
             y_test <- y[test_set]
             start <- Sys.time()
             mod <- do.call(what = model, args = c(list(x = x_train, y = y_train), param_list))
@@ -142,6 +142,67 @@ textmodel_evaluate.tokens <- function(x, y, model, fun = "f1_score", k = 5,
             met <- lapply(fun, function(x) do.call(what = x, args = list(y_pred, y_test, by_class))) # Accepts any evaluation function that takes predicted and test vectors as inputs
             #met <- as.list(met)
             if (is.null(names(met))) names(met) <- fun
+            if (length(parameters) != 0) {
+                output[[w]] <- data.frame(k = i, met, param_list, as.list(time), seed)
+            } else {
+                output[[w]] <- data.frame(k = i, met, as.list(time), seed)
+            }
+            if (by_class) {
+                output[[w]]$class <- rownames(output[[w]])
+            }
+            w <- w + 1
+        }
+    }
+    output <- do.call(rbind, output)
+    rownames(output) <- NULL
+    total_time <- round(as.numeric(difftime(Sys.time(), total_start, units = "secs")), 2) # Outputs time in seconds
+    attr(output, "model") <- model
+    attr(output, "fun") <- fun
+    attr(output, "k") <- k
+    attr(output, "parameters") <- parameters
+    attr(output, "nparameters") <- param_len
+    attr(output, "total_time") <- total_time
+    class(output) <- c("textmodel_evaluate", "data.frame")
+    return(output)
+}
+
+#' @export
+textmodel_evaluate.tokens2sequences <- function(x, y, model, fun = "f1_score", k = 5,
+                                   parameters = list(),
+                                   seed = as.numeric(Sys.time()),
+                                   time = TRUE, by_class = FALSE) {
+    stopifnot(is.tokens2sequences(x))
+    if ("accuracy" %in% fun & by_class) {
+        cat("No class oriented accuracy score defined. Calculating average accuracy accross all classes.\n")
+    }
+    total_start <- Sys.time()
+    set.seed(seed)
+    y <- as.factor(y)
+    folds <- cut(seq(1, length(y)), breaks = k, labels = FALSE)
+    folds <- sample(folds, length(folds), replace = FALSE)
+    output <- list()
+    params_df <- expand.grid(parameters, stringsAsFactors = FALSE)
+    param_len <- ifelse(length(parameters) != 0, nrow(params_df), 1)
+    w <- 1
+    for (t in 1:param_len) {
+        # drop = FALSE ensures that params_df remains a data.frame even if
+        # there is only a single input parameter
+        param_list <- as.list(params_df[t, , drop = FALSE])
+        for (i in 1:k) {
+            train_set <- which(folds != i)
+            test_set <- which(folds == i)
+            x_train <- tokens2sequences_subset(x, train_set)
+            x_test <- tokens2sequences_subset(x, test_set)
+            y_train <- y[-test_set]
+            y_test <- y[test_set]
+            start <- Sys.time()
+            mod <- do.call(what = model, args = c(list(x = x_train, y = y_train), param_list))
+            time <- round(as.numeric(difftime(Sys.time(), start, units = "secs")), 2) # Outputs time in seconds
+            names(time) <- "time"
+            y_pred <- predict(object = mod, newdata = x_test, type = 'class')
+            met <- lapply(fun, function(x) do.call(what = x, args = list(y_pred, y_test, by_class))) # Accepts any evaluation function that takes predicted and test vectors as inputs
+            #met <- as.list(met)
+            if ( is.null(names(met))) names(met) <- fun
             if (length(parameters) != 0) {
                 output[[w]] <- data.frame(k = i, met, param_list, as.list(time), seed)
             } else {
