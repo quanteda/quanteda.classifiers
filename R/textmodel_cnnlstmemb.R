990f244ca7ef97768a3a561af9f7ec46e7b0a2ed
Patrick Chester <pjc468@nyu.edu>
2021-06-13 16:35:08 -0400

    Update textmodel_cnnlstmemb.R
    Make trainable a parameter

--- R/textmodel_cnnlstmemb.R
+++ R/textmodel_cnnlstmemb.R
@@ -25,6 +25,7 @@
 #' @param fitted_embeddings A fitted embeddings model formatted such that the 
 #'   columns include a word identifier and embedding dimensions.The rows should 
 #'   represent individual tokens.
+#' @param trainable If true, fitted embeddings are fitted to training text
 #' @param optimizer optimizer used to fit model to training data, see
 #'   [keras::compile.keras.engine.training.Model()]
 #' @param loss objective loss function, see
@@ -66,7 +67,7 @@ textmodel_cnnlstmemb <- function(x, y, dropout = 0.2,filter = 48,
                                  kernel_size = 5, pool_size = 4, units_lstm = 128, 
                                  words = NULL,maxsenlen = NULL,
                                  wordembeddim = 30, cnnlayer = TRUE,
-                                 fitted_embeddings = NULL,
+                                 fitted_embeddings = NULL, trainable = NULL,
                                  optimizer = "adam",
                                  loss = "categorical_crossentropy",
                                  metrics = "categorical_accuracy", ...) {
@@ -78,7 +79,7 @@ textmodel_cnnlstmemb.tokens <- function(x, y, dropout = 0.2,filter = 48,
                                  kernel_size = 5, pool_size = 4, units_lstm = 128, 
                                  words = NULL, maxsenlen = NULL,
                                  wordembeddim = 30, cnnlayer = TRUE,
-                                 fitted_embeddings = NULL, 
+                                 fitted_embeddings = NULL, trainable = NULL,
                                  optimizer = "adam",
                                  loss = "categorical_crossentropy",
                                  metrics = "categorical_accuracy", ...) {
@@ -107,9 +108,9 @@ textmodel_cnnlstmemb.tokens <- function(x, y, dropout = 0.2,filter = 48,
         fitted_embeddings <- replace(fitted_embeddings, is.na(fitted_embeddings), 0) %>% 
             as.matrix() %>% 
             list()
-        trainable <- FALSE
+        if(is.null(trainable)) trainable <- FALSE
     } else {
-        trainable <- TRUE
+        if(is.null(trainable)) trainable <- TRUE
     }
     
     # "one-hot" encode y
@@ -131,13 +132,14 @@ textmodel_cnnlstmemb.tokens <- function(x, y, dropout = 0.2,filter = 48,
         model %>%
             layer_conv_1d(filters = filter, kernel_size = kernel_size,
                           activation = "relu") %>%
-            layer_max_pooling_1d(pool_size = pool_size) %>%
-            layer_dropout(rate = dropout)
+            layer_max_pooling_1d(pool_size = pool_size)# %>%
+            #layer_dropout(rate = dropout)
     }
     
     model %>%
-        layer_lstm(units = units_lstm, dropout = dropout,
-                                 recurrent_dropout = dropout) %>%
+        layer_lstm(units = units_lstm#, dropout = dropout,
+                                 #recurrent_dropout = dropout
+                                 ) %>%
         layer_dense(units = nlevels(y), activation = "softmax")
     
     compile(model, loss = loss, optimizer = optimizer, metrics = metrics)
@@ -158,7 +160,7 @@ textmodel_cnnlstmemb.tokens2sequences <- function(x, y, dropout = 0.2,filter = 4
                                  kernel_size = 5, pool_size = 4, units_lstm = 128, 
                                  words = NULL, maxsenlen = NULL,
                                  wordembeddim = 30, cnnlayer = TRUE,
-                                 fitted_embeddings = NULL, 
+                                 fitted_embeddings = NULL, trainable = NULL,
                                  optimizer = "adam",
                                  loss = "categorical_crossentropy",
                                  metrics = "categorical_accuracy", ...) {
@@ -191,9 +193,9 @@ textmodel_cnnlstmemb.tokens2sequences <- function(x, y, dropout = 0.2,filter = 4
         fitted_embeddings <- replace(fitted_embeddings, is.na(fitted_embeddings), 0) %>% 
             as.matrix() %>% 
             list()
-        trainable <- FALSE
+        if(is.null(trainable)) trainable <- FALSE
     } else {
-        trainable <- TRUE
+        if(is.null(trainable)) trainable <- TRUE
     }
     # use keras to fit the model
     #model <- cnnlstm_model(x, y2)
@@ -211,13 +213,14 @@ textmodel_cnnlstmemb.tokens2sequences <- function(x, y, dropout = 0.2,filter = 4
         model %>%
             layer_conv_1d(filters = filter, kernel_size = kernel_size,
                           activation = "relu") %>%
-            layer_max_pooling_1d(pool_size = pool_size) %>%
-            layer_dropout(rate = dropout)
+            layer_max_pooling_1d(pool_size = pool_size) #%>%
+            #layer_dropout(rate = dropout)
     }
     
     model %>%
-        layer_lstm(units = units_lstm, dropout = dropout,
-                                 recurrent_dropout = dropout) %>%
+        layer_lstm(units = units_lstm#, 
+                   #dropout = dropout, recurrent_dropout = dropout
+                   ) %>%
         layer_dense(units = nlevels(y), activation = "softmax")
     
     compile(model, loss = loss, optimizer = optimizer, metrics = metrics)
@@ -323,7 +326,6 @@ print.predict.textmodel_cnnlstmemb <- function(x, ...) {
 #' @rdname save.textmodel_mlp
 #' @importFrom keras serialize_model
 #' @method save textmodel_cnnlstmemb
-#' @export
 save.textmodel_cnnlstmemb <- function(x, ...) {
     x$clefitted <- serialize_model(x$clefitted)
     save(x, ...)
@@ -332,7 +334,6 @@ save.textmodel_cnnlstmemb <- function(x, ...) {
 #' @rdname save.textmodel_mlp
 #' @importFrom keras unserialize_model
 #' @method load textmodel_cnnlstmemb
-#' @export
 load.textmodel_cnnlstmemb <- function(x, ...) {
     load(x, ...)
     x$clefitted <- unserialize_model(x$clefitted)
